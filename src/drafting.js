a3603710bb811e09d0af2abbf5ab2576bd9a02f2
Felipe Assunção <felipe.assuncao@corp.nova>
2016-07-27 19:26:23 -0300

    examples

--- src/drafting.js
+++ src/drafting.js
@@ -1,34 +1,6 @@
 // Exporting the module.
 var inverter = require('./inverter');
 
-// inverter.RegisterType("a", function() {
-//
-//     this.log = function() {
-//         console.log('dependencia aaaaaaaa');
-//     };
-//
-//     return {
-//         log: this.log
-//     };
-// });
-// inverter.RegisterType("instance", function(a) {
-//
-//   //  console.log(a);
-//
-//     //a.log();
-//
-//     return {
-//         leiba: 'samba leiba'
-//     };
-//
-// }, true);
-//
-//
-// console.log(inverter.CheckClass(function () {
-//
-// }));
-
-
 inverter.RegisterType('Constants', function() {
   this.pi = 3.14159;
   return this;
